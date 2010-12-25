62796e0c24344d2b37cd664ad4de635e510ab7ea
Sho Hashimoto <hashimoto@shokai.org>
2010-12-25 17:48:44 +0900

    fix build/debug

--- debug.sh
+++ debug.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 name=interaction2011
 (platex-utf8 -shell-escape $name &&
-    platex-utf8 $name  &&
-    platex-utf8 $name  &&
     dvipdfmx $name &&
     open $name".pdf") || echo 'error!'
 
